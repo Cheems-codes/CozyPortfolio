d0320415fd880d39f6b1abf4a3bfc67eec513a49
Cheems-codes <olbeseurico@gmail.com>
2026-09-05 13:57:35 +0800

    Fix GitHub Pages base routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 
 function Router() {
   return (
-    <WouterRouter base={import.meta.env.BASE_URL}>
+    <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
       <Switch>
         <Route path={"/"} component={Home} />
         <Route path={"/404"} component={NotFound} />
